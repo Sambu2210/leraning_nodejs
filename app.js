1ee900da543cd8c6b06766e6f981a6e2567fe755
Sambu2210 <sanjai007007007@gmail.com>
2023-07-25 11:11:21 +0530

    once the request comes then writing the result

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const http = require("http");
 //   console.log(req);
 // }
 const server = http.createServer((req, res) => {
-  console.log(res);
+  res.write("<html>");
+  res.write("<head><title>Learn about node.js</title></head>");
+  res.write("<body><h1>Learn about node.js</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
